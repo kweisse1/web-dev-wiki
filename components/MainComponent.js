449681a51c82775f1c0c8a372ce3e7d5c60cab58
Ryan Snodgrass <rsnodgra92@gmail.com>
2021-03-10 10:57:50 -0700

    Clean up and comments added

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,17 +1,13 @@
 
 import React from 'react';
 import { StyleSheet} from 'react-native';
-import {ReactPage, ReactNativePage, SassPage, BootstrapPage, HomePage, InfoDisplay} from "./pages"
+import {HomePage, InfoDisplay} from "./pages"
 import { createStackNavigator } from 'react-navigation-stack';
 import { createAppContainer } from 'react-navigation';
 
 const MainNavigator = createStackNavigator(
   {
       HomePage: { screen: HomePage },
-      ReactPage: { screen: ReactPage}, 
-      ReactNativePage: {screen: ReactNativePage},
-      BootstrapPage: {screen: BootstrapPage}, 
-      SassPage: {screen: SassPage}, 
       InfoDisplayPage: {screen: InfoDisplay}
   }, 
   {
--- components/pages/InfoDisplay.js
+++ components/pages/InfoDisplay.js
@@ -2,6 +2,7 @@ import React, {Component} from "react"
 import {View, Text} from 'react-native';
 import pagesDisplayArr from "../../shared/data.js"
 
+//every wiki-info page should  be displayed via this component
 class InfoDisplay extends Component
 {
   constructor(props)
@@ -12,6 +13,7 @@ class InfoDisplay extends Component
     }
   }
  
+  // sets stack navigator header details, these params are defined and passed in through the homepage component.
   static navigationOptions = ({ navigation }) => ({
     title: navigation.state.params.title,
     headerRight: navigation.state.params.docsButton
@@ -20,6 +22,7 @@ class InfoDisplay extends Component
   render()
   {
     return(
+      // The look and content of the page is defined in shared/data.js
       <View><Text>{this.state.pageObject.view}</Text></View>
     )
   }
