2bf7e98c0f3f35707054ae1b71ba0ee4d07c8630
Ryan Snodgrass <rsnodgra92@gmail.com>
2021-03-16 19:33:06 -0600

    Minor changes to improve readability

--- components/techSummaries/ReactNativeTechSummary.js
+++ components/techSummaries/ReactNativeTechSummary.js
@@ -4,8 +4,12 @@ import {Paragraph} from "../../components/"
 function ReactNativeTechSummary(props)
 {
 return(
-
- <Paragraph text="React Native leverages the power of React to create mobile applications. One major benefit to writing applications in this way is that the codebase can be written once and run on both the iOS and Android platforms. React does this by mapping JavaScript to native code. While this method of app development has its advantages, it does come with its downsides as well. Native apps will have better performance than those written in React Native. React native is written in a similar fashion to React but has different components. "/>
+<>
+ <Paragraph text={`             React Native leverages the power of React to create mobile applications. While very similar in structure to React, React Native utilizes a different set of components.
+ `}/>
+ 
+ <Paragraph text={`             One major benefit to writing applications in this way is that the codebase can be written once and run on both the iOS and Android platforms. React does this by mapping JavaScript to native code. While this method of app development has its advantages, it does come with its downsides as well. Native apps will have better performance than those written in React Native.`}/>
+</>
 )
 
 }
--- components/techSummaries/ReactNavigationTechSummary.js
+++ components/techSummaries/ReactNavigationTechSummary.js
@@ -4,8 +4,9 @@ import {Paragraph} from "../../components/"
 function ReactNavigationTechSummary(props)
 {
 return(
-
- <Paragraph text="React Navigation is a library that provides developers tools to set up internal navigation of a React Native application. In a similar manner to how React Router provides developers tools to navigate a react application. To this end React Navigation offers stack, tab and drawer navigation among others."/>
+<>
+ <Paragraph text={`             React Navigation is a library that provides developers tools to set up internal navigation of a React Native application. In a similar manner to how React Router provides developers tools to navigate a react application. To this end React Navigation offers stack, tab and drawer navigation among others.`}/>
+</>
 )
 
 }
